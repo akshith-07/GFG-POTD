073bdb1dcdc56058f302a2bb27b155ea644fa96a
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2025-12-29 08:28:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,76 +1,22 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
 class Solution {
   public:
-    int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
-        if(arr1.size() > arr2.size()){
-            return kthElement(k, arr2, arr1);
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
+        
+        int i = 0, j = 0;
+        int ans = 0;
+        while(k > 0 && i < a.size() && j < b.size()){
+            
+            if(a[i] < b[j])     ans = a[i++];
+            else                ans = b[j++];
+
+            k--;
+            if(k == 0)  return ans; 
         }
         
-        int n1 = arr1.size();
-        int n2 = arr2.size();
-        int low = max(0, k - n2);
-        int high = min(n1, k);
+        if(i < a.size())  return a[i + k - 1];
         
-        while(low <= high) {
-            int mid1 = (low + high) / 2;
-            int mid2 = k - mid1;
+        if(j < b.size())  return b[j + k - 1];
             
-            int l1 = (mid1 == 0) ? INT_MIN : arr1[mid1 - 1];
-            int l2 = (mid2 == 0) ? INT_MIN : arr2[mid2 - 1];
-            int r1 = (mid1 == n1) ? INT_MAX : arr1[mid1];
-            int r2 = (mid2 == n2) ? INT_MAX : arr2[mid2];
-            
-            if(l1 <= r2 && l2 <= r1) {
-                return max(l1, l2);
-            } else if(l1 > r2) {
-                high = mid1 - 1;
-            } else {
-                low = mid1 + 1;
-            }
-        }
-        return -1;
+    return -1;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-// Driver code
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, m, k;
-        cin >> k;
-        cin.ignore();
-        string input;
-        int num;
-        vector<int> arr1, arr2;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            arr1.push_back(num);
-        }
-
-        getline(cin, input);
-        stringstream s3(input);
-        while (s3 >> num) {
-            arr2.push_back(num);
-        }
-
-        Solution ob;
-        cout << ob.kthElement(k, arr1, arr2) << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends

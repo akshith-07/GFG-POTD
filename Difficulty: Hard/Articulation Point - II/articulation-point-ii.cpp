14c1ea1101e69a7cb563efcb537a48a836cf3b81
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2026-03-28 21:07:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Articulation Point - II/articulation-point-ii.cpp
+++ Difficulty: Hard/Articulation Point - II/articulation-point-ii.cpp
@@ -1,103 +1,58 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
 public:
+    void dfs(int node, int parent, vector<int> adj[], vector<int> &vis,
+             vector<int> &tin, vector<int> &low, vector<int> &mark, int &timer) {
+        
+        vis[node] = 1;
+        tin[node] = low[node] = timer++;
+        int child = 0;
+
+        for (auto it : adj[node]) {
+            if (it == parent) continue;
+
+            if (!vis[it]) {
+                dfs(it, node, adj, vis, tin, low, mark, timer);
+                
+                low[node] = min(low[node], low[it]);
+
+                if (low[it] >= tin[node] && parent != -1) {
+                    mark[node] = 1;
+                }
+                child++;
+            } 
+            else {
+                low[node] = min(low[node], tin[it]);
+            }
+        }
+
+        if (parent == -1 && child > 1) {
+            mark[node] = 1;
+        }
+    }
+
     vector<int> articulationPoints(int V, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(V);
-        for (auto& e : edges) {
+        
+        vector<int> adj[V];
+        for (auto &e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
 
-        vector<bool> visited(V, false);
-        vector<int> disc(V, -1);
-        vector<int> low(V, -1);
-        vector<int> parent(V, -1);
-        vector<bool> ap(V, false);
-        int time = 0;
+        vector<int> vis(V, 0), tin(V), low(V), mark(V, 0);
+        int timer = 0;
 
-        for (int i = 0; i < V; ++i) {
-            if (!visited[i]) {
-                dfs(i, adj, visited, disc, low, parent, ap, time);
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(i, -1, adj, vis, tin, low, mark, timer);
             }
         }
 
-        vector<int> result;
-        for (int i = 0; i < V; ++i) {
-            if (ap[i]) {
-                result.push_back(i);
-            }
+        vector<int> ans;
+        for (int i = 0; i < V; i++) {
+            if (mark[i]) ans.push_back(i);
         }
 
-        if (result.empty()) {
-            return {-1};
-        }
-
-        return result;
-    }
-
-private:
-    void dfs(int u, vector<vector<int>>& adj, vector<bool>& visited, vector<int>& disc, vector<int>& low, vector<int>& parent, vector<bool>& ap, int& time) {
-        visited[u] = true;
-        disc[u] = low[u] = ++time;
-        int children = 0;
-
-        for (int v : adj[u]) {
-            if (v == parent[u]) {
-                continue;
-            }
-
-            if (!visited[v]) {
-                parent[v] = u;
-                children++;
-                dfs(v, adj, visited, disc, low, parent, ap, time);
-
-                low[u] = min(low[u], low[v]);
-
-                if (parent[u] != -1 && low[v] >= disc[u]) {
-                    ap[u] = true;
-                }
-            } else {
-                low[u] = min(low[u], disc[v]);
-            }
-        }
-
-        if (parent[u] == -1 && children >= 2) {
-            ap[u] = true;
-        }
+        if (ans.empty()) return {-1};
+        return ans;
     }
 };
- 
-
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> edges;
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            edges.push_back({u, v});
-        }
-        Solution obj;
-        vector<int> ans = obj.articulationPoints(V, edges);
-        sort(ans.begin(), ans.end());
-        for (auto i : ans)
-            cout << i << " ";
-        cout << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends

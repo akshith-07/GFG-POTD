cf271c4e9f40a798e8333815462b2aefe3f2509c
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2026-03-30 20:28:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.cpp
+++ Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.cpp
@@ -1,74 +1,61 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-  public:
+public:
+    int find(int d,vector<int> &p){
+           if(p[d] == d){
+               return d;
+           }
+           return find(p[d],p);
+    }
     int minCost(vector<vector<int>>& houses) {
-        // code here
+        //cost of "connecting" i.e. DSU will apply here.
+        int cost = 0;
         int n = houses.size();
-        
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        
-        vector<int> vis(n, 0);
-        pq.push({0, 0});
-        int sum =0; 
-        
-        while(!pq.empty()){
-            auto node = pq.top();
-            pq.pop();
-            int wt = node.first;
-            int ind = node.second;
-            if (vis[ind]) continue;
-    
-            vis[ind] = 1;
-            sum += wt;
-    
-            for (int i = 0; i < n; i++) {
-                if (!vis[i]) {
-                    int newWt = abs(houses[ind][0] - houses[i][0]) + abs(houses[ind][1] - houses[i][1]);
-                    pq.push({newWt, i});
+        vector<int> p(n);
+        for(int i = 0;i<n;i++){
+            p[i] = i;
+        }
+        map<pair<int,int>,int> mp;
+        for(int i = 0;i<n;i++){
+            mp[{houses[i][0],houses[i][1]}] = i;
+        }
+        vector<int> r(n,0);
+        vector<pair<int,pair<int,int>>> v;
+        for(int i = 0;i<n;i++){
+        for(int j = i+1;j<n;j++){
+            int a = houses[i][0];
+            int b = houses[i][1];
+            int c = houses[j][0];
+            int d = houses[j][1];
+            int sum = abs(d-b)+abs(c-a);
+            v.push_back({sum,{mp[{a,b}],mp[{c,d}]}});
+        }
+        }
+        sort(v.begin(),v.end());
+        for(int i = 0;i<v.size();i++){
+            int a = v[i].first;
+            int b = v[i].second.first;
+            int c = v[i].second.second;
+            int x = find(b,p);
+            int y = find(c,p);
+            if(x == y){
+              continue;
+            }
+            else{
+                cost += a;
+                if(r[x] <= r[y]){
+                    p[x] = y;
+                    if(r[y] < r[x] + 1){
+                       r[y] = r[x] + 1;
+                    }
+                }
+                else{
+                    p[y] = x;
+                    if(r[x] < r[y] + 1){
+                       r[x] = r[y] + 1;
+                    }
                 }
             }
         }
-        
-        return sum;
+        return cost;
     }
 };
-
-
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> edges;
-
-        for (int i = 0; i < n; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < 2; ++j) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            edges.push_back(temp);
-        }
-
-        Solution obj;
-
-        cout << obj.minCost(edges);
-        cout << "\n";
-        cout << "~" << endl;
-    }
-}
-
-// } Driver Code Ends

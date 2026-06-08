7ec92912bef228788a5032f867a93793c605ccb3
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2026-06-08 19:47:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Nodes with Greater on Right/delete-nodes-with-greater-on-right.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    Node *compute(Node *head) {
+        // code here
+        stack<Node*> s;
+        
+        while(head != nullptr) {
+            if(s.empty() || head->data < s.top()->data) {
+                if(!s.empty()) {
+                    s.top()->next = head;   
+                }
+                s.push(head);
+            } else {
+                while(!s.empty() && head->data > s.top()->data) {
+                    s.pop();
+                }
+                if(!s.empty()) {
+                    s.top()->next = head;   
+                }
+                s.push(head);
+            }
+            head = head->next;
+        }
+        
+        Node* ans = nullptr;
+        while(!s.empty()) {
+            ans = s.top();
+            s.pop();
+        }
+        
+        return ans;
+    }
+};
